92516fe7411fa11b10835822f5021a962698a4a3
HenryNgu1 <henryn.contact@gmail.com>
2025-11-08 16:36:58 +1100

    Update: configure passport + session middlwares

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
-import { PrismaClient } from '@prisma/client';
 import session from 'express-session'
 import connectPgSimple from 'connect-pg-simple'
+import { configureSession } from './config/session.js';
+import { configurePassport } from './config/passport.js';
 const PgSession = connectPgSimple(session);
-const prisma = new PrismaClient();
-
 
 //1. Create express app instance
 const app = express();
@@ -20,58 +18,22 @@ app.use(express.json());
 //3. Middleware to parse cookies
 app.use(cookieParser());
 
-// Ensure session secret is not null
-const SESSION_SECRET = process.env.SESSION_SECRET;
-if (!SESSION_SECRET) {
-    throw new Error('SESSION_SECRET environment variable is required');
-}
-// Connect session to DB
-const sessionStore = new PgSession({
-    conString: process.env.DATABASE_URL,
-        tableName: 'session'
-})
-
 //4. Session middleware -> initialises a session and set cookie header with SID
-app.use(session({
-    store: sessionStore,
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,     
-    cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-        //ms, sec, min, hour, day
-    }
-}));
+app.use(configureSession());
 
-//5. passport middleware -> attach helper functions to req
+//5. Initialize passport
+configurePassport();
+//6. passport middleware -> attach helper functions to req
 app.use(passport.initialize());
-
-//6. passport middleware session -> on every req calls deserializeUser 
+//7. passport middleware session -> on every req calls deserializeUser 
 app.use(passport.session());
 
-// Serialize user into the sesssion
-passport.serializeUser((user: any, done: (err: any, id?: any) => void) => {
-    done(null, user.id)
-})
-
-// Deserialize user from the session
-passport.deserializeUser(async (id: string, done: (err: any, user?: any) => void) => {
-    try {
-        const user = await prisma.user.findUnique({ where: { id } });
-        done(null, user || false);
-    } catch (err) {
-        done(err);
-    }
-});
-
-//7. Basic route to root for testing
+//8. Basic route to root for testing
 app.get('/' , (req: express.Request, res: express.Response) => {   
     res.send('Express app is running');
 });
 
-//8. Error handling middleware for middlewares
+//9. Error handling middleware for middlewares
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
---
+++ src/config/passport.ts
@@ -0,0 +1,71 @@
+import passport from 'passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+// New prisma client -> connection to DB
+const prisma = new PrismaClient();
+
+export const configurePassport = () => {
+
+    // Serialize user ID for the session
+    passport.serializeUser((user: any, done) => {
+        done(null, user.id); 
+
+        // When user logs in passport saves only user ID to session table
+        // Sends the cookie to the browser with session ID
+    })
+
+    // Deserialize user from the session 
+    passport.deserializeUser(async (id: string, done) => {
+        try { 
+            const user = await prisma.user.findUnique({
+                where: {id}
+            });
+            done(null, user);
+        } catch (error) {
+            done(error);      
+        }
+
+        // On new requests the cookie is sent from client with session ID
+        // We receive and look up the userID that matches the ID from the session
+    });
+
+    // Setup google oAuth strategy
+    passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL!
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          // Find or create user
+          let user = await prisma.user.findUnique({
+            where: { auth_user_id: profile.id }
+          });
+
+          if (!user) {
+            // Create new user if not found
+            user = await prisma.user.create({
+                data: {
+                    auth_user_id: profile.id,
+                    name: profile.displayName,
+                    avartar_url: profile.photos?.[0]?.value ?? null,
+                    timezone: 'UTC', 
+                    settings: {} as Prisma.JsonObject
+                }
+            });
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error as Error);
+        }
+      }
+    )
+
+    // Passport handles log in using google sign in
+    // When user signs in callback method recieves the user profile adds or loads user 
+  );
+}
